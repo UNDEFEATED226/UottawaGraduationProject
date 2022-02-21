11d9311b2b718f4b617d9da1fc31b6f30c601502
ShizeMation <1732145@gmail.com>
2022-02-20 21:28:50 -0500

    Pages now uses display grid, fix sidebar collapse

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,14 @@
 import './App.css'
 import { Component } from 'react';
 import SideBar from './components/SideBar'
+import Header from './components/Header'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic voluptatibus obcaecati suscipit voluptatem consectetur in nemo sunt earum non at praesentium placeat ut ratione explicabo nisi, molestiae perspiciatis. Nobis, animi?
-        </header>
-        <div className='SideBarContainer'>
-          <SideBar selected="MyProducts"/>
-        </div>
+        <Header username="Joe"/>
+        <SideBar selected="MyProducts"/>
         <div className='mainSection'>
           <p>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime consequatur accusamus nam tenetur temporibus ad sunt necessitatibus aperiam delectus perspiciatis porro illo, expedita unde, nemo iusto veniam, magni vero eaque?
