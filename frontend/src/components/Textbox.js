fa1465385889efbbf0fceefac894b87881b35b47
ShizeMation <1732145@gmail.com>
2022-02-27 19:56:06 -0500

    Added Textbox component

---
+++ frontend/src/components/Textbox.js
@@ -0,0 +1,22 @@
+import './Textbox.css'
+import PropTypes from "prop-types";
+
+const Textbox = ({name, labelText, placeholderText, required, disabled}) => {
+    
+    return (
+        <div className="Textbox">
+            <label for={name}>{labelText}</label>
+            <input type="text" id={name} name={name} placeholder={placeholderText} required={required} disabled={disabled}/>
+        </div>
+    );
+}
+
+Textbox.propTypes = {
+    name: PropTypes.string.isRequired,
+    labelText: PropTypes.string.isRequired,
+    placeholderText: PropTypes.string,
+    required: PropTypes.bool,
+    disabled: PropTypes.bool
+}
+ 
+export default Textbox;
