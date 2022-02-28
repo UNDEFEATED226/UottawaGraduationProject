14868d6df2c63c5ca2b6e9ddc2b68384363b6b78
ShizeMation <1732145@gmail.com>
2022-02-27 20:01:38 -0500

    Added Routing

---
+++ frontend/src/AboutMe.js
@@ -0,0 +1,12 @@
+const AboutMe = () => {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias accusamus, quasi ipsam recusandae sed sequi id amet quia officiis laudantium. Error at saepe ullam possimus omnis minus modi rem doloribus enim nobis quis iste adipisci, ducimus doloremque. Quisquam accusamus consectetur tempore illum perferendis, provident rerum quo porro laboriosam dolorum veniam repellat autem dolorem odit atque magni qui! Soluta ducimus ipsa nihil explicabo sunt aspernatur cum rem? Rerum, necessitatibus eveniet quam facilis consequuntur magnam laborum molestiae aut quasi sapiente neque. Dolore distinctio maxime ut atque sed alias maiores exercitationem fugit placeat suscipit quasi praesentium fuga, architecto aliquam mollitia totam voluptatibus similique officia magnam! Esse aperiam recusandae enim neque natus, possimus nemo voluptatum explicabo deserunt ipsum ad consequatur placeat alias et modi officiis id sequi? Doloremque quidem a voluptatem debitis consequuntur, ea maxime? Esse minima eveniet voluptatibus consectetur. Unde, perspiciatis voluptatem ipsa nobis alias consectetur quod error officiis. Cupiditate dignissimos id pariatur explicabo maiores, nihil voluptatibus voluptatem eius distinctio nam soluta non ad nobis vero quos debitis totam temporibus cum impedit consectetur ab veniam? Nobis est porro quasi harum architecto quisquam. Accusamus reprehenderit sint dolor deserunt mollitia ullam ratione, veritatis corrupti aliquid ab nisi labore ipsum a unde eaque odit provident nesciunt.
+            </p>
+        </div>
+    );
+}
+
+export default AboutMe;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,21 @@
 import './App.css'
 import { Component } from 'react';
-import SideBar from './components/SideBar'
-import Header from './components/Header'
-import Checkbox from './components/Checkbox';
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import SideBar from './components/SideBar';
+import BasicInformation from 'BasicInformation';
+import AboutMe from 'AboutMe';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header username="Joe"/>
-        <SideBar selected="MyProducts"/>
-        <div className='mainSection'>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima beatae optio fugiat neque odio blanditiis? Necessitatibus dolor nobis labore doloribus nesciunt excepturi ullam hic minus quam harum! Iure, dolor aperiam quidem illum officiis, nesciunt dolorum adipisci vero voluptates alias nisi veniam cum repellendus unde labore maiores nobis eos, commodi temporibus? Quia rem architecto nisi natus odit? Maxime quaerat beatae alias quas ratione architecto sint ducimus ab, consequuntur consequatur provident aspernatur sapiente rerum unde eligendi totam voluptates. Eum, eos quo. Incidunt totam corporis ipsum tenetur quibusdam laborum illum qui nesciunt adipisci ex blanditiis, praesentium, delectus quaerat odio debitis porro ducimus sapiente ut neque iure culpa consequatur? A neque architecto, itaque laudantium ratione fugit, veniam earum minus iure nemo debitis, expedita quisquam autem repudiandae amet necessitatibus sapiente explicabo molestiae eaque error recusandae animi? Odio suscipit corporis consequuntur, possimus sint assumenda, amet similique labore quae dignissimos nisi doloribus impedit a tenetur repudiandae nulla eos laboriosam. In eum quidem debitis amet deleniti beatae, quod ipsam hic labore, quam dolor consequatur, at aspernatur eius incidunt libero exercitationem illo atque? Aperiam, voluptatum aliquid! Vero exercitationem consequuntur architecto aperiam modi nesciunt porro atque. Id itaque expedita impedit ullam ab eaque necessitatibus, unde nam recusandae quisquam aperiam adipisci?
-          </p>
-          <Checkbox name='Test Checkbox'/>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel sunt praesentium velit assumenda ad obcaecati! Quidem impedit error ullam, blanditiis sit cupiditate. Architecto ipsam illum animi! Facere sequi dolor voluptatum aliquam dolores ipsam, explicabo recusandae assumenda? Sapiente atque itaque, nihil, reprehenderit rerum ex dolorum modi harum ad laborum corrupti sed quibusdam! Facilis temporibus eius odit ex alias, explicabo velit asperiores deleniti debitis mollitia maxime nesciunt corporis. Similique sequi voluptatibus error quo est sunt perferendis nostrum asperiores provident, fuga minus aut minima quis dolores illum id odio at qui unde voluptas atque sint delectus? A pariatur illo ab rem optio, nemo excepturi aspernatur suscipit expedita iure illum magni incidunt quibusdam aut tenetur saepe voluptatem ipsam praesentium quia, quidem totam eos! Magnam, aspernatur, animi temporibus nulla error rem amet odio deleniti dolor totam, quos cum quae at veniam! Assumenda blanditiis sit labore minima praesentium, magnam id ut aliquam perferendis accusantium est voluptatum delectus rerum repellat dolores amet incidunt aut ipsam culpa laudantium. Debitis consectetur itaque ex ab incidunt tempora nam non vero? Perferendis, nostrum aut similique voluptas facilis possimus autem sed, fugit modi cumque, repudiandae libero sequi. Harum, saepe! Quisquam ut magni recusandae iure at provident, velit odit iusto nihil delectus id?
-          </p>
-        </div>
+        <SideBar selected="BasicInfo"/>
+        <Routes>
+          <Route path="/" element={<BasicInformation />} />
+          <Route path="/aboutme" element={<AboutMe />} />
+        </Routes>
       </div>
     );
   }
