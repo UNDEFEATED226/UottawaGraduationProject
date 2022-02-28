12effb76d592c9eab8d6cc9734b9277b93613fd8
ShizeMation <1732145@gmail.com>
2022-02-27 19:56:33 -0500

    Added Textarea component

---
+++ frontend/src/components/Textarea.js
@@ -0,0 +1,31 @@
+import './Textarea.css';
+import PropTypes from "prop-types";
+
+const Textarea = ({name, labelText, placeholderText, required, disabled, rows, cols}) => {
+    
+    return (
+        <div className='Textarea'>
+            <label for={name}>{labelText}</label>
+            <textarea 
+                   id={name} name={name} 
+                   placeholder={placeholderText} 
+                   required={required} 
+                   disabled={disabled}
+                   rows={rows}
+                   cols={cols}
+            />
+        </div>
+    );
+}
+
+Textarea.propTypes = {
+    name: PropTypes.string.isRequired,
+    labelText: PropTypes.string.isRequired,
+    placeholderText: PropTypes.string,
+    required: PropTypes.bool,
+    disabled: PropTypes.bool,
+    row: PropTypes.number,
+    cols: PropTypes.number,
+}
+ 
+export default Textarea;
