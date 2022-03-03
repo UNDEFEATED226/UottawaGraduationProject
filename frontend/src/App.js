4fd300d4549a1470822256a8c969c4971454cf7a
ShizeMation <1732145@gmail.com>
2022-03-03 18:34:24 -0500

    Added MyProducts page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,20 @@
 import './App.css'
 import { Component } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import SideBar from './components/SideBar';
-import BasicInformation from 'BasicInformation';
+import Header from 'components/Header';
+import SideBar from 'components/SideBar';
+import BasicInformation from 'pages/BasicInformation';
+import MyProducts from 'pages/MyProducts';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header username="Joe"/>
-        <SideBar selected="BasicInfo"/>
+        <SideBar defaultSelect="BasicInfo"/>
         <Routes>
           <Route path="/" element={<BasicInformation />} />
+          <Route path="/my_products" element={<MyProducts />} />
         </Routes>
       </div>
     );
