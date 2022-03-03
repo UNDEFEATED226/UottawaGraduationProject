8e16cbc77946156e780ddc12a9a8a58e70d98945
ShizeMation <1732145@gmail.com>
2022-03-03 18:33:56 -0500

    Made SideBar functional with Links

--- frontend/src/components/SideBar.js
+++ frontend/src/components/SideBar.js
@@ -1,36 +1,47 @@
 import './SideBar.css'
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-const SideBar = ({ selected }) => {
+const SideBar = ({ defaultSelect }) => {
 
     const [collapsed, setCollapsed] = useState(false);
+    const [selected, setSelected] = useState(defaultSelect);
 
     return (
         <div className={"SideBar" + (collapsed? " collapsed" : "")}>
-            <button onClick={() => setCollapsed(!collapsed)}>
+            <button id='collapse-btn' onClick={() => setCollapsed(!collapsed)}>
                 {collapsed? ">" : "< Collapse"}
             </button>
-            <div className={selected === "BasicInfo"? "selected" : "unselected"}>
-                Basic Information
-            </div>
-            <div className={selected === "AboutMe"? "selected" : "unselected"}>
-                About Me
-            </div>
-            <div className={selected === "MyProducts"? "selected" : "unselected"}>
-                My Products
-            </div>
-            <div className={selected === "MyGrants"? "selected" : "unselected"}>
-                My Grants
-            </div>
-            <div className={selected === "MyEvents"? "selected" : "unselected"}>
-                My Events
-            </div>
-            <div className={selected === "MySupervisions"? "selected" : "unselected"}>
-                My Supervisions
-            </div>
-            <div className={selected === "Partners"? "selected" : "unselected"}>
-                Partners
-            </div>
+            <Link to='/' onClick={() => setSelected('BasicInfo')}>
+                <div className={selected === "BasicInfo"? "selected" : ""}>
+                    Basic Information
+                </div>
+            </Link>
+            <Link to='/my_products' onClick={() => setSelected('MyProducts')}>
+                <div className={selected === "MyProducts"? "selected" : ""}>
+                    My Products
+                </div>
+            </Link>
+            <Link to='/my_grants' onClick={() => setSelected('MyGrants')}>
+                <div className={selected === "MyGrants"? "selected" : ""}>
+                    My Grants
+                </div>
+            </Link>
+            <Link to='/my_events' onClick={() => setSelected('MyEvents')}>
+                <div className={selected === "MyEvents"? "selected" : ""}>
+                    My Events
+                </div>
+            </Link>
+            <Link to='/my_supervisions' onClick={() => setSelected('MySupervisions')}>
+                <div className={selected === "MySupervisions"? "selected" : ""}>
+                    My Supervisions
+                </div>
+            </Link>
+            <Link to='/partners' onClick={() => setSelected('Partners')}>
+                <div className={selected === "Partners"? "selected" : ""}>
+                    Partners
+                </div>
+            </Link>
             <div></div>
         </div>
     );
