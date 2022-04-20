be5ca5b155bbe8a0e9d39cc5fdcf7b32ea482778
ShizeMation <1732145@gmail.com>
2022-04-20 19:53:09 -0400

    Textbox now supports numbers and formatters

--- frontend/src/components/Textbox.js
+++ frontend/src/components/Textbox.js
@@ -2,31 +2,55 @@ import './Textbox.css';
 import PropTypes from "prop-types";
 import { useEffect, useState } from 'react';
 
-const Textbox = ({name, labelText, text, placeholderText, errorMessage, required, disabled, onChange}) => {
+const Textbox = ({name, labelText, text, isPassword, isNumber, formatter,
+    placeholderText, errorMessage, required, disabled, onChange}) => {
 
-    const [fieldText, setFieldText] = useState('');
+    const [displayText, setDisplayText] = useState('');
+    const [focused, setFocused] = useState(false);
 
     useEffect(() => {
-        setFieldText(text);
-    }, [text]);
+        if (!focused) {
+            if (formatter) setDisplayText(formatter(text));
+            else setDisplayText(text);
+        }
+    }, [text, formatter, focused]);
 
     const handleChange = e => {
-        setFieldText(e.target.value);
-        onChange(name, e.target.value);
+        let value = e.target.value;
+        if (isNumber) {
+            if (value !== '' && !Number(value)) return;
+            setDisplayText(value);
+            onChange(name, Number(value));
+        }
+        else {
+            setDisplayText(value);
+            onChange(name, value);
+        }
+    }
+
+    const handleFocus = () => {
+        setFocused(true);
+        setDisplayText(text);
+    }
+
+    const handleBlur = () => {
+        setFocused(false);
     }
 
     return (
         <div className="Textbox">
             <label htmlFor={name}>{labelText}</label>
             <input
-                type="text"
+                type={isPassword ? "password" : "text"}
                 id={name}
                 name={name}
-                value={fieldText ?? ""}
+                value={displayText ?? ""}
                 placeholder={placeholderText}
                 required={required}
                 disabled={disabled}
                 onChange={handleChange}
+                onFocus={handleFocus}
+                onBlur={handleBlur}
             />
             {errorMessage && <span className='errorMsg'>{errorMessage}</span>}
         </div>
@@ -36,12 +60,32 @@ const Textbox = ({name, labelText, text, placeholderText, errorMessage, required
 Textbox.propTypes = {
     name: PropTypes.string.isRequired,
     labelText: PropTypes.string.isRequired,
-    text: PropTypes.string,
+    text: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number
+    ]),
+    isPassword: PropTypes.bool,
+    isNumber: PropTypes.bool,
+    formatter: PropTypes.func,
     placeholderText: PropTypes.string,
     errorMessage: PropTypes.string,
     required: PropTypes.bool,
     disabled: PropTypes.bool,
     onChange: PropTypes.func,
+}
+
+Textbox.defaultProps = {
+    name: '',
+    labelText: '',
+    text: '',
+    isPassword: false,
+    isNumber: false,
+    formatter: null,
+    placeholderText: null,
+    errorMessage: null,
+    required: false,
+    disabled: false,
+    onChange: null,
 }
 
 export default Textbox;
